888dcef8995e710a797e3fbac59abe159a45183c
larven313 <hidayatullahsukma@gmail.com>
2024-03-25 10:58:44 +0700

    Praktikum pertemuan 4 - Props & Media

--- movie-cinema/src/App.js
+++ movie-cinema/src/App.js
@@ -1,42 +1,52 @@
 import { Component } from "react";
+import Header from "./component/Header.js";
+import Car from "./component/Car.js";
+import User from "./component/User.js";
+import Footer from "./component/Footer.js";
+import imgProfile from "./component/man.jpg";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Hello World</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
-        assumenda dignissimos incidunt. Aut sit quia sint. Eum molestias
-        similique veniam quia necessitatibus a fuga incidunt doloribus et
-        recusandae? Repellendus, commodi?
-      </p>
-      <Footer />
+      <Car
+        merk="Ferrari"
+        harga={5}
+        isNew={true}
+        colors={["Hitam", "Putih", "Abu-abu"]}
+        beli={() => alert("Sudah dibeli")}
+      />
+      {/* <User
+        nama="Bambang Pacul"
+        alamat="Bandung"
+        angkatan={10}
+        hobi={["Ngoding", "Memancing"]}
+        isGraduate={false}
+        gambar={imgProfile}
+        message={() => alert("Profil nama telah dibuka")}
+      />
+      <User
+        nama="Budi"
+        alamat="Bandung"
+        angkatan={10}
+        hobi={["Memasak", "Bermain bola"]}
+        isGraduate={false}
+        gambar="https://cdn-icons-png.flaticon.com/512/7718/7718888.png"
+        message={() => alert("Profil nama telah dibuka")}
+      />
+      <User
+        nama="Ucup"
+        alamat="Sukabumi"
+        angkatan={10}
+        hobi={["Ngoding", "Memancing"]}
+        isGraduate={false}
+        gambar="https://cdn-icons-png.freepik.com/256/1077/1077063.png"
+        message={() => alert("Profil nama telah dibuka")}
+      /> */}
+      <Footer nama="Sukma Hidayatullah" />
     </div>
   );
-}
-
-const Header = () => {
-  return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Login</li>
-      </ul>
-    </nav>
-  );
-};
-
-class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <h3>Copyright &copy;2024 Developed by Sukma Hidayatullah</h3>
-        <span>Make with &#10084</span>
-      </footer>
-    );
-  }
 }
 
 export default App;
