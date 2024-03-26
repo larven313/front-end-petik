f2c257cdc92770e1fd957c4e482bdca0476b7263
larven313 <hidayatullahsukma@gmail.com>
2024-03-26 11:04:59 +0700

    Praktikum pertemuan 5 - Styling

--- movie-cinema/src/App.js
+++ movie-cinema/src/App.js
@@ -1,49 +1,23 @@
 import { Component } from "react";
-import Header from "./component/Header.js";
-import Car from "./component/Car.js";
-import User from "./component/User.js";
-import Footer from "./component/Footer.js";
-import imgProfile from "./component/man.jpg";
+import Header from "./component/Navbar/Navbar.js";
+import Car from "./component/Car/Car.js";
+import User from "./component/User/User.js";
+import Footer from "./component/Footer/Footer.js";
+import Main from "./component/Main/Main.js";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header />
-      <h1>Hello World</h1>
-      <Car
+      {/* <h1>Hello World</h1> */}
+      {/* <Car
         merk="Ferrari"
         harga={5}
         isNew={true}
         colors={["Hitam", "Putih", "Abu-abu"]}
         beli={() => alert("Sudah dibeli")}
-      />
-      {/* <User
-        nama="Bambang Pacul"
-        alamat="Bandung"
-        angkatan={10}
-        hobi={["Ngoding", "Memancing"]}
-        isGraduate={false}
-        gambar={imgProfile}
-        message={() => alert("Profil nama telah dibuka")}
-      />
-      <User
-        nama="Budi"
-        alamat="Bandung"
-        angkatan={10}
-        hobi={["Memasak", "Bermain bola"]}
-        isGraduate={false}
-        gambar="https://cdn-icons-png.flaticon.com/512/7718/7718888.png"
-        message={() => alert("Profil nama telah dibuka")}
-      />
-      <User
-        nama="Ucup"
-        alamat="Sukabumi"
-        angkatan={10}
-        hobi={["Ngoding", "Memancing"]}
-        isGraduate={false}
-        gambar="https://cdn-icons-png.freepik.com/256/1077/1077063.png"
-        message={() => alert("Profil nama telah dibuka")}
       /> */}
+      <Main />
       <Footer nama="Sukma Hidayatullah" />
     </div>
   );
